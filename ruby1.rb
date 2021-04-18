5561169275a6b45402b24acdb4e68a4b7594aafc
kay-orijinal <kay.0122.27.soccer@icloud.com>
2021-04-18 13:05:38 +0900

    second

--- ruby1.rb
+++ ruby1.rb
@@ -1,4 +1,4 @@
-require "csv"
+require 'csv'
 
 puts "1(新規でメモを作成) 2(既存のメモを編集)"
 
@@ -6,28 +6,26 @@ memo_type = gets.to_i
 
 if memo_type == 1
   puts "拡張子を除いたファイル名を入力してください"
-  fail_name = gets.to_s
+  file_name = gets.chomp
   
   puts "メモの内容を入力してください"
   puts "入力後は、control+Dで保存できます"
-  memo_contens = gets.to_s
+  memo_contens = readlines.map(&:chomp)
   
-  CVS.open('#{fail_name}.csv','w') do |fail|
-    fail.puts memo_contens
+  CSV.open("#{file_name}.csv",'w') do |memo|
+    memo << memo_contens
   end
 elsif memo_type == 2
   puts "拡張子を除いたファイル名を入力してください"
-  fail_name = gets.to_s
+  file_name = gets.chomp
   
   puts "メモの追加内容を入力してください"
   puts "入力後は、control+Dで保存できます"
-  memo_contens = gets.to_s
+  memo_contens = readlines.map(&:chomp)
   
-  CVS.open('#{fail_name}.csv','a') do |fail|
-    fail.puts memo_contens
+  CSV.open("#{file_name}.csv",'a') do |memo|
+    memo << memo_contens
   end
 else
-  puts "もう一度入力してください"
-  puts "1(新規でメモを作成) 2(既存のメモを編集)"
-  memo_type = gets.to_i
+  puts "もう一度やり直してください"
 end
